113e76f1b5de64e084e523bfd8bd8c7fb6c2c546
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2016-03-14 23:09:40 +0000

    Updated the projectname

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return username == 'impactgen' and password == 'remote2016'
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
